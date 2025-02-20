b23508e1b80db5ca2094e5067647c42ce9913561
潘学森 <panxuesen@tojoy.com>
2025-02-20 14:02:36 +0800

    docs: 更新文章页面的 git 信息
    - 为多个文章页面添加 git 更新时间戳和贡献者信息
    - 修改首页 base 路径为 "/blob/"

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
   lang: 'en-US',
-
+  base: '/blob/',
   title: 'VuePress',
   description: 'My first VuePress Site',
 
